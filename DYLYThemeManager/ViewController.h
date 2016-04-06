324cd38b57f9b260d6076f000304674ba7abd59a
XuYanci <glengedix@gmail.com>
2016-04-06 17:35:46 +0800

    Initial Commit

---
+++ DYLYThemeManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYLYThemeManager
+//
+//  Created by Yanci on 16/4/6.
+//  Copyright © 2016年 Yanci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DYLYThemeManagerTests/DYLYThemeManagerTests.m
@@ -0,0 +1,39 @@
+//
+//  DYLYThemeManagerTests.m
+//  DYLYThemeManagerTests
+//
+//  Created by Yanci on 16/4/6.
+//  Copyright © 2016年 Yanci. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DYLYThemeManagerTests : XCTestCase
+
+@end
+
+@implementation DYLYThemeManagerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
